802571f28dcabb29505a119c05f5f82f777c6054
polinayermolenko <shvajukova@mail.ru>
2020-06-10 23:51:08 +0300

    Add class 'done' to the item of the list

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
+  state = {
+    done: false
+  };
+  
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    })
+  };
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done'; 
+    }
+
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
 
     return (
-      <span className='todo-list-item'>
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
           style={style}
-          onClick={ ()=> console.log(`Done: ${label}`)}>
+          onClick={ this.onLabelClick }>
           {label}
         </span>
 
@@ -30,6 +47,6 @@ export default class TodoListItem extends Component {
         </button>
       </span>
     );
-  }
+  };
 };
 
