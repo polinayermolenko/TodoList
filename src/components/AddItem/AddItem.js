412606e3ca8e26b55fb1ff64524774dfea22fe20
polinayermolenko <shvajukova@mail.ru>
2020-06-18 11:50:49 +0300

    Clear form

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import './AddItem.css';
 
 export default class AddItem extends Component {
+    state = {
+      label: ''
+    };
+
+    onLabelChange = (evt) => {
+       this.setState({
+         label: evt.target.value
+       });
+    };
+    
+    onSubmit = (evt) => {
+      evt.preventDefault();
+      this.props.onItemAdded(this.state.label);
+      this.setState({
+          label: ''
+      });
+    };
 
     render() {
-        const { onItemAdded } = this.props;
+        
         return (
-            <div className='add-item' >
-                <button className='btn btn-outline-secondary' onClick={() => onItemAdded('hi')}>Add Item</button>
-            </div>
+            <form className='add-item d-flex' onSubmit={this.onSubmit}>
+                <input type='text' className='form-control' 
+                 onChange={this.onLabelChange}
+                 placeholder='What needs to be done?'
+                 value={this.state.label}/>
+                <button className='btn btn-outline-secondary'>
+                    Add Item
+                </button>
+            </form>
         )
     }
 }
