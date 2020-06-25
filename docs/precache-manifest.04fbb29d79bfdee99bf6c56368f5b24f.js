c3a2aac5b305dd4e3e7ae8a048918e25d4ab1ad2
polinayermolenko <shvajukova@mail.ru>
2020-06-26 00:09:48 +0300

    for GitHub Pages

---
+++ docs/precache-manifest.04fbb29d79bfdee99bf6c56368f5b24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcee9be7051fc8969a0bfe2fe6525db6",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "14536d17ed20ab9a16a3",
+    "url": "/TodoList/static/css/main.6c60c05f.chunk.css"
+  },
+  {
+    "revision": "3fe9f8a1d4fade360c43",
+    "url": "/TodoList/static/js/2.731454ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.731454ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14536d17ed20ab9a16a3",
+    "url": "/TodoList/static/js/main.8d252f31.chunk.js"
+  },
+  {
+    "revision": "fdcb04d4c9b8f2448d6d",
+    "url": "/TodoList/static/js/runtime-main.ca153b22.js"
+  }
+]);
